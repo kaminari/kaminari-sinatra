cfd992ab56c581fd7c59d44a13ef9e221193f541
Akira Matsuda <ronnie@dio.jp>
2016-04-01 05:14:10 +0900

    Sinatra is not Rails

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,6 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-begin
-  require 'rails'
-rescue LoadError
-end
-
 require 'bundler/setup'
 Bundler.require
 require 'active_record'
@@ -15,12 +10,6 @@ require 'database_cleaner'
 
 # Simulate a gem providing a subclass of ActiveRecord::Base before the Railtie is loaded.
 require 'fake_gem' if defined? ActiveRecord
-
-if defined? Rails
-  require 'fake_app/rails_app'
-
-  require 'rspec/rails'
-end
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
